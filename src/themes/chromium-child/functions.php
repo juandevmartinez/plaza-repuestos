20636a21cffdf01efc70a816afb40bab40c69b1f
Juan Martinez <juan.martinez@sigma3ds.com>
2021-03-09 09:24:02 -0500

    remove affiliate products

--- src/themes/chromium-child/functions.php
+++ src/themes/chromium-child/functions.php
@@ -25,3 +25,44 @@
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version'));
  }
  add_action('wp_enqueue_scripts', 'chromium_child_enqueue');
+
+ function add_meta_user_registration( $user_id ){
+    $user = get_userdata ( $user_id );
+    // Update the registration meta data
+    update_user_meta ( $user_id, 'registration_date', $user->user_registered );
+ }
+ add_action('user_register', 'add_meta_user_registration' , 10, 1);
+
+ function get_approved_date( $user_id ){
+   return get_user_meta( $user_id, 'approved_date', true );
+ };
+
+ function update_meta_on_change( $user_id, $role, $old_roles ){
+
+   switch( $role ){
+      case 'dc_vendor':
+         register_approved_date( $user_id );
+         break;  
+   }
+
+ }
+ add_action( 'set_user_role', 'update_meta_on_change', 10, 3 );
+
+ function var_view( $data ){
+    echo '<pre>' . var_dump( $data ) . '</pre>';
+ }
+
+ function register_approved_date( $user_id ){
+      $date = strtotime("now");
+      return update_user_meta( $user_id, 'approved_date', $date  );
+ }
+
+
+ /**
+  * Remove External Products 
+  */
+ add_filter( 'product_type_selector', 'remove_product_types' );
+ function remove_product_types( $types ){
+   unset( $types['external'] );
+   return $types;
+ }
